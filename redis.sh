3afb5fde6cbfd614fdbb7e2971f98a01f7fd8553
galantixa <galantixaa@gmail.com>
2023-11-01 11:23:41 +0700

    ordent-tech-test

---
+++ redis.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# run as root
+if [ "$EUID" -ne 0 ]; then
+    echo "must be root"
+    exit 1
+fi
+
+# prompt untuk memasukkan port
+read -p "Input PORT: " PORT
+PORT=${PORT:-6379}
+
+# download and install
+cd /tmp
+wget http://download.redis.io/releases/redis-7.0.0.tar.gz
+tar xzf redis-7.0.0.tar.gz
+cd redis-7.0.0
+make
+make install
+
+# create user redis
+sudo adduser --system --group --no-create-home redis
+
+# create dir for redis data
+mkdir -p /var/lib/redis/$PORT
+cp redis.conf /var/lib/redis/$PORT.conf
+
+#  mengganti nilai port
+sed -i "s/port 6379/port $PORT/" /var/lib/redis/$PORT.conf
+# untuk mengubah direktori tempat redis menyimpan data
+sed -i "s|dir ./|dir /var/lib/redis/$PORT/|" /var/lib/redis/$PORT.conf
+
+chown -R redis:redis /var/lib/redis
+# start redis
+systemctl restart redis@$PORT
